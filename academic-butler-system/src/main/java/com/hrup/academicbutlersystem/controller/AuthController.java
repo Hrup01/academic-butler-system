425069b69f46e5b7b0b58d5c3ac5177b04570313
kk <1448714441@qq.com>
2025-05-29 16:57:05 +0800

    更新了统一的相应数据，添加了springboot-web依赖

--- academic-butler-system/src/main/java/com/hrup/academicbutlersystem/controller/AuthController.java
+++ academic-butler-system/src/main/java/com/hrup/academicbutlersystem/controller/AuthController.java
@@ -1,4 +1,19 @@
 package com.hrup.academicbutlersystem.controller;
 
+import com.hrup.academicbutlersystem.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 登录认证控制器
+ *
+ * */
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+
 }
